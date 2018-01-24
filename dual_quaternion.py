Reprojection error visualisation, fixes to Quaternion.mat

dual_quaternion.py
@@ -2,20 +2,33 @@ import numpy as np
 
 class Quaternion:
     """
-    Quaternion:
-        q = w+ x*i + y*j + z*k
+    Quaternion
+        q = w + x*i + y*j + z*k
+    Can be initialised with:
+        - 4 arguments w, x, y, z
+        - list or numpy array with 4 elements w, x, y, z
+        - or Quaternion(angle = a, axis = b), a is angle in degrees and b is
+        3 element list or numpy array. Axis is normalised, and a unit
+        quaternion is initialised
+        - default initialised as 1*w + 0*i + 0*j + 0*k
     """
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
         if len(args) == 4:
             self.w = args[0]
             self.x = args[1]
             self.y = args[2]
             self.z = args[3]
-        elif len(args) == 1 and len(args[0]) == 4:
+        elif len(args) == 1 and isinstance(args[0], (list, np.ndarray)):
             self.w = args[0][0]
             self.x = args[0][1]
             self.y = args[0][2]
             self.z = args[0][3]
+        elif 'angle' in kwargs.keys() and 'axis' in kwargs.keys():
+            n = kwargs['axis']/np.linalg.norm(kwargs['axis'])
+            self.w = np.cos(np.radians(kwargs['angle'])/2)
+            self.x = n[0]*np.sin(np.radians(kwargs['angle'])/2)
+            self.y = n[1]*np.sin(np.radians(kwargs['angle'])/2)
+            self.z = n[2]*np.sin(np.radians(kwargs['angle'])/2)
         else:
             self.w = 1
             self.x = 0
@@ -23,15 +36,15 @@ class Quaternion:
             self.z = 0
 
     def __str__(self):
-        return "{} + {}*i + {}*j + {}*k".format(self.w, self.x, self.y, self.z)
-
+        return '{} + {}i + {}j + {}k'.format(self.w, self.x, self.y, self.z)
+    
     def __repr__(self):
-        return "Quaternion({}, {}, {}, {})".format(
+        return 'Quaternion({}, {}, {}, {})'.format(
             self.w, self.x, self.y, self.z)
 
     def __add__(self, other):
         if type(self) != type(other):
-            raise TypeError("Must add two quaternions.")
+            raise TypeError('Must add two quaternions.')
         return Quaternion(self.w + other.w, self.x + other.x,
             self.y + other.y, self.z + other.z)
 
@@ -42,7 +55,7 @@ class Quaternion:
                 other*self.z)
         elif type(self) != type(other):
             raise TypeError(
-                "Must multiply quaternion with real number or quaternion.")
+                'Must multiply quaternion with real number or quaternion.')
 
         w = self.scalar*other.scalar - np.dot(self.vector, other.vector)
         xyz = (self.scalar*other.vector + other.scalar*self.vector +
@@ -61,6 +74,21 @@ class Quaternion:
     def __neg__(self):
         return Quaternion(-self.w, -self.x, -self.y, -self.z)
 
+    def normalize(self):
+        return self.unit
+
+    @property
+    def unit(self):
+        return self*(1/self.norm)
+
+    @property
+    def axis(self):
+        return self.unit.vector/(np.sin(np.radians(self.angle)/2))
+
+    @property
+    def angle(self):
+        return np.degrees(2*np.arccos(self.unit.w))
+
     @property
     def scalar(self):
         return self.w
@@ -74,8 +102,57 @@ class Quaternion:
         return Quaternion(self.w, -self.x, -self.y, -self.z)
 
     @property
+    def inverse(self):
+        return self.conjugate*(1/self.norm**2)
+
+    @property
     def norm(self):
         return np.sqrt(self.w**2 + self.x**2 + self.y**2 + self.z**2)
+
+    @property
+    def mat(self):
+        """Returns 3x3 rotation matrix. Normalizes quaternion first."""
+        q0 = self.unit.w; qx = self.unit.x; qy = self.unit.y; qz = self.unit.z
+        return np.array([[q0**2+ qx**2 -qy**2 -qz**2 ,2*(qx*qy - q0*qz),
+                            2*(qx*qz+q0*qy)],
+                         [2*(qx*qy + q0*qz),(q0**2 - qx**2 + qy**2 - qz**2),
+                            2*(qy*qz - q0*qx)],
+                         [2*(qz*qx - q0*qy),2*(qz*qy + q0*qx),
+                            (q0**2 - qx**2 - qy**2 + qz**2)]])
+
+    @property
+    def hmat(self):
+        """Returns 4x4 homogeneous rotation matrix."""
+        H = np.eye(4)
+        H[:3,:3] = self.mat
+        return H
+
+    @property
+    def euler(self):
+        """
+        Returns Euler angle representation, assumes ZYX ordering, returns
+        array in format [rX, rY, rZ] in degrees.
+        """
+        R = self.mat
+        sy = -R[2,0]
+        cy = 1-(sy*sy)
+
+        if cy > 0.00001:
+            cy = np.sqrt(cy)
+            cx = R[2,2]/cy
+            sx = R[2,1]/cy
+            cz = R[0,0]/cy
+            sz = R[1,0]/cy
+        else:
+            cy = 0.0
+            cx = R[1,1]
+            sx = -R[1,2]
+            cz = 1.0
+            sz = 0.0
+        return np.array([np.degrees(np.arctan2(sx, cx)),
+            np.degrees(np.arctan2(sy, cy)),
+            np.degrees(np.arctan2(sz, cz))])
+
 
 class DualNumber:
     """
@@ -87,63 +164,83 @@ class DualNumber:
         self.dual = dual
 
     def __repr__(self):
-        return "dual_number({}, {}).".format(self.real, self.dual)
+        return "{}({}, {}).".format(
+            self.__class__.__name__, repr(self.real), repr(self.dual))
 
     def __str__(self):
         return "({}) + eps*({})".format(self.real, self.dual)
 
+    def __add__(self, other):
+        if type(self) != type(other):
+            raise TypeError('Must add two dual numbers of same type.')
+        return self.__class__((self.real + other.real),
+            (self.dual + other.dual))
+
+    def __sub__(self, other):
+        return self.__class__(self.real + (-other.real),
+            self.dual + (-other.dual))
+
+    def __pos__(self):
+        return self.__class__(self.real, self.dual)
+
+    def __neg__(self):
+        return self.__class__(-self.real, -self.dual)
+
+    def __mul__(self, other):
+        """self*x, where x is int, float or dual number"""
+        if isinstance(other, (int, float)):
+            return self.__class__(self.real*other, self.dual*other)
+        elif type(self) != type(other):
+            raise TypeError(
+            'Must multiply dual number with real number of dual number' +
+            'of same type.')
+        return self.__class__(self.real*other.real,
+            (self.real*other.dual + self.dual*other.real))
+
+    def __rmul__(self, other):
+        """x*self, where x is int or float."""
+        if isinstance(other, (int, float)):
+            return self.__class__(self.real*other, self.dual*other)
+        else:
+            raise TypeError(
+                'Must multiply dual number with real number of dual number' +
+                'of same type.')
+
+
 class DualQuaternion(DualNumber):
     """
-    Dual quaternions are dual numbers where both real and dual parts are
-    quaternions.
+    Dual quaternions are dual numbers q + eps*q', where q and q' are quaternions.
     """
     def __init__(self, *args):
         if len(args) == 8:
             super().__init__(Quaternion([args[0], args[1], args[2], args[3]]),
                              Quaternion([args[4], args[5], args[6], args[7]]))
-        elif (len(args) == 1) and (len(args[0]) == 8):
+        elif (len(args) == 1) and isinstance(args[0], (list, np.ndarray)):
             super().__init__(Quaternion([args[0][0], args[0][1], args[0][2],
                                        args[0][3]]),
                              Quaternion([args[0][4], args[0][5], args[0][6],
                                        args[0][7]]))
-        elif len(args) == 2:
+        elif len(args) == 2 and all(isinstance(i, (list, np.ndarray)) for
+            i in args):
+            super().__init__(Quaternion(args[0]), Quaternion(args[1]))
+        elif len(args) == 2 and all(isinstance(i, (Quaternion)) for i in args):
             super().__init__(args[0], args[1])
         else:
-            super().__init__()
+            super().__init__(Quaternion(), Quaternion())
 
-    def __repr__(self):
-        return "DualQuaternion({}, {})".format(repr(self.real),
-            repr(self.dual))
+    @property
+    def norm(self):
+        return DualNumber(self.real*self.real.conjugate,
+            self.real*self.dual.conjugate + self.dual*self.real.conjugate)
 
+    @property
     def mat(self):
         """
         Returns 4x4 homogenous matrix representation of self.
         """
         H = np.eye(4)
-
-        # q0 = self.real[0]
-        # qx = self.real[1]
-        # qy = self.real[2]
-        # qz = self.real[3]
-        q0 = self.real.w
-        qx = self.real.x
-        qy = self.real.y
-        qz = self.real.z
-
-        R = np.array([[q0**2+qx**2-qy**2-qz**2,2*(qx*qy-q0*qz),
-                       2*(qx*qz+q0*qy)],
-                      [2*(qx*qy*q0*qz),(q0**2-qx**2+qy**2-qz**2),
-                       2*(qy*qz-q0*qx)],
-                      [2*(qz*qx-q0*qy),2*(qz*qy+q0*qx),
-                       (q0**2-qx**2-qy**2+qz**2)]])
-
-        # q = self.real; q[1:] = -self.real[1:]
-        q = self.real.conjugate
-        # t = 2*qmult(self.dual,q)
-        t = 2*(self.dual*q)
-        H[:3,:3] = R
-        #H[:3,3] = t[1:]
-        H[:3,3] = t.vector
+        H = self.real.hmat
+        H[:3,3] = 2*(self.dual*self.real.conjugate).vector
         return H
 
     @property
@@ -152,10 +249,13 @@ class DualQuaternion(DualNumber):
 
     @property
     def scalar(self):
-        """Returns scalar part."""
-        sc_real = 0.5*(self.real + self.conj.real)
-        sc_dual = 0.5*(self.dual + self.conj.dual)
-        return dual_number(sc_real.scalar, sc_dual.scalar)
+        """Returns scalar (quaternion) dual number."""
+        return DualNumber(self.real.scalar, self.dual.scalar)
+
+    @property
+    def vector(self):
+        """Returns vector (quaternion) dual number."""
+        return DualNumber(self.real.vector, self.dual.vector)
 
     @property
     def screw_angle(self):
@@ -168,13 +268,8 @@ class DualQuaternion(DualNumber):
         theta = self.screw_angle
         return (-2*self.scalar.dual)/np.sin(theta/2)
 
-def hom2dq(H):
-    """Converts a 4x4 homogeneous, rigid body transformation matrix H into a
-    dual quaternion."""
-
-    R = H[:3,:3]
-    t = H[:3,3]
-
+def rot2q(R):
+    """Converts 3x3 rotation matrix to quaternion"""
     q0 = 1 + R[0,0] + R[1,1] + R[2,2]
     qx = 1 + R[0,0] - R[1,1] - R[2,2]
     qy = 1 - R[0,0] + R[1,1] - R[2,2]
@@ -182,7 +277,7 @@ def hom2dq(H):
     qCheck = [q0,qx,qy,qz]
 
     if np.max(qCheck) == qx:
-        qx = -np.sqrt(qx/4)
+        qx = np.sqrt(qx/4)
         qy = (R[1,0]+R[0,1])/(4*qx)
         qz = (R[0,2]+R[2,0])/(4*qx)
         q0 = (R[2,1]-R[1,2])/(4*qx)
@@ -201,12 +296,15 @@ def hom2dq(H):
         qx = (R[2,1]-R[1,2])/(4*q0)
         qy = (R[0,2]-R[2,0])/(4*q0)
         qz = (R[1,0]-R[0,1])/(4*q0)
+    return Quaternion(q0, qx, qy, qz)
 
-    dq_real = np.array([q0,qx,qy,qz])
-    t_q = np.array([0,t[0],t[1],t[2]])
-    dq_dual = 0.5*qmult(t_q,dq_real)
-
-    return DualQuaternion(Quaternion(dq_real), Quaternion(dq_dual))
+def hom2dq(H):
+    """Converts a 4x4 homogeneous, rigid body transformation matrix H into a
+    dual quaternion."""
+    R = H[:3,:3]; t = H[:3,3]
+    q1 = rot2q(R)
+    q2 = 0.5*Quaternion(0, t[0], t[1], t[2])*q1
+    return DualQuaternion(q1, q2)
 
 def qmult(q1,q2):
     """Quaternion multiplication."""
@@ -291,7 +389,7 @@ def dqcrosscalib(A,B):
         if Xdq.real.scalar < 0:
             Xdq.real = -Xdq.real
             Xdq.dual = -Xdq.dual
-    return Xdq.mat()
+    return Xdq.mat
 
 """Testing"""
 if __name__ == "__main__":

leopard_imaging.py
@@ -7,6 +7,8 @@ import glob
 import os
 import re
 from checkerboard_motion import pixel_reprojections
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def split_images(path):
     """
@@ -83,6 +85,7 @@ def calibrate_leopard_stereo(directory, check_rows, check_cols, length,
     imgpoints_right = []
 
     images = glob.glob(os.path.join(directory, "*.{}".format(pixel_format)))
+    imageNames = []
 
     if not images:
         print("No images found in {}".format(directory))
@@ -105,7 +108,7 @@ def calibrate_leopard_stereo(directory, check_rows, check_cols, length,
                                                    (check_rows, check_cols),
                                                    None)
 
-        if (ret1 and ret2):
+        if (ret1 and ret2): # Process only if corners found for both views
             corners1 = cv2.cornerSubPix(grey_left, corners1, (5,5), (-1,-1),
                                         criteria)
             corners2 = cv2.cornerSubPix(grey_right, corners2, (5,5), (-1,-1),
@@ -113,6 +116,7 @@ def calibrate_leopard_stereo(directory, check_rows, check_cols, length,
             objpoints.append(objp)
             imgpoints_left.append(corners1)
             imgpoints_right.append(corners2)
+            imageNames.append(os.path.splitext(os.path.split(image)[1])[0])
 
             if showCorners:
                 cv2.drawChessboardCorners(img_left,
@@ -141,8 +145,8 @@ def calibrate_leopard_stereo(directory, check_rows, check_cols, length,
                                                  img_left.shape[:2],None,None)
 
 
-    print("Mean reprojection error for left camera: ", ret1)
-    print("Mean reprojection error for right camera: ", ret2)
+    print("Mean reprojection error for left camera (no stereo cal): ", ret1)
+    print("Mean reprojection error for right camera (no stero cal): ", ret2)
 
     left_reproj, left_pix = pixel_reprojections(check_rows, check_cols,
                 objpoints, imgpoints_left, K1, rv1, tv1, dc1)
@@ -158,6 +162,35 @@ def calibrate_leopard_stereo(directory, check_rows, check_cols, length,
                                         img_left.shape[:2],
                                         cv2.CALIB_FIX_INTRINSIC)
 
+    # Show reprojection errors for each image.
+    rms_left = np.zeros(len(imageNames))
+    rms_right = np.zeros(len(imageNames))
+    for image, i in zip(imageNames, range(len(imageNames))):
+        rM1, _ = cv2.Rodrigues(rv1[i]) # Convert rot vec to matrix
+        rv2_new = np.dot(R, rM1)
+        rv2_new, _ = cv2.Rodrigues(rv2_new)
+        tv2_new = T + np.dot(R, tv1[i])
+        left_reproj, _ = cv2.projectPoints(
+            objpoints[i], rv1[i], tv1[i], K1, dc1)
+        right_reproj, _ = cv2.projectPoints(
+            objpoints[i], rv2_new, tv2_new, K2, dc2)
+        errs_left = np.squeeze(imgpoints_left[i] - left_reproj)
+        errs_right = np.squeeze(imgpoints_right[i] - right_reproj)
+        rms_left[i] = np.mean(np.sqrt((
+            errs_left[:,0]**2 + errs_left[:,1]**2)))
+        rms_right[i] = np.mean(np.sqrt((
+            errs_right[:,0]**2 + errs_right[:,1]**2)))
+
+    df = pd.DataFrame(np.array([rms_left, rms_right]).T,
+                      columns = ['Left', 'Right'],
+                      index = imageNames)
+    df.plot.bar()
+    plt.ylabel('Mean reprojection error (pixels)')
+    plt.title('Stereo calibration mean reprojection error')
+    plt.show()
+
+    np.savez('Objpoints.npz', objpoints = objpoints,
+        left = imgpoints_left, right = imgpoints_right)
     print("Final mean reprojection error: ", retval)
     np.savez("Stereo_calibration",
              retval = retval,
@@ -178,6 +211,125 @@ def calibrate_leopard_stereo(directory, check_rows, check_cols, length,
 
     print("Calibration finished, results saved.")
     return
+
+def correct_dist(vec,fc,c,k,p):
+    """
+    From Andre's IDL code.
+    Inputs:
+        vec - Nx2 array of distorted pixel coordinates.
+        fc  - 2 element focal length.
+        c   - principal point.
+        k   - 2 (or 3) radial distortion coefficients.
+        p   - 2 tangential distortion coefficients.
+    Outputs:
+        cvec - Nx2 array of distortion-corrected pixel coordinates.
+    """
+    # May be 2 or 3 radial coefficients depending on calibration method
+    if len(k) == 3:
+        k3 = k[2]
+    else:
+        k3 = 0
+
+    ud = vec[:,0]
+    vd = vec[:,1]
+    xn = (ud - c[0])/fc[0] # Normalise points
+    yn = (vd - c[1])/fc[1]
+    x = xn
+    y = yn
+
+    for i in range(20):
+        r2 = x*x + y*y
+        r4 = r2*r2
+        r6 = r4*r2
+        k_radial = 1 + k[0]*r2 + k[1]*r4 + k3*r6
+        delta_x = 2*p[0]*x*y + p[1]*(r2 + 2*x*x)
+        delta_y = 2*p[1]*x*y + p[0]*(r2 + 2*y*y)
+        x = (xn - delta_x)/k_radial
+        y = (yn - delta_y)/k_radial
+
+    x = fc[0]*x + c[0] # Undo normalisation
+    y = fc[1]*y + c[1]
+    cvec = np.array([x,y]).T
+    return cvec
+
+def get_checkerboard_points3d(directory, check_rows, check_cols,
+    P1, P2, fc1, fc2, dc1, dc2, pp1, pp2, ext = '.pgm'):
+    """
+    Processes images in directory, returning the average 3d position of
+    checkerboard points
+    """
+    # Termination criteria for getting sub-pixel corner positions.
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    # Prepare corner points, e.g. (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((check_rows*check_cols,3), np.float32)
+    # Variable 'length' is size of checkerboard square.
+    objp[:,:2] = np.mgrid[0:check_rows,0:check_cols].T.reshape(-1,2)
+    objpoints = []
+    imgpoints_left = [] # 2d points in image plane.
+    imgpoints_right = []
+
+    for f in os.scandir(directory):
+        if os.path.splitext(f.name)[1] == ext:
+            img = cv2.imread(os.path.join(directory, f.name))
+            img_left = img[:,640:]
+            img_right = img[:,:640]
+            grey_left = cv2.cvtColor(img_left, cv2.COLOR_BGR2GRAY)
+            grey_right = cv2.cvtColor(img_right, cv2.COLOR_BGR2GRAY)
+
+            # Find corners.
+            # cv2.findChessboardCorners(img, patternsize(rows, columns))
+            ret1, corners1 = cv2.findChessboardCorners(grey_left,
+                                                   (check_rows, check_cols),
+                                                   None)
+            ret2, corners2 = cv2.findChessboardCorners(grey_right,
+                                                   (check_rows, check_cols),
+                                                   None)
+
+            if (ret1 and ret2):
+                corners1 = cv2.cornerSubPix(grey_left, corners1, (5,5),
+                                            (-1,-1),
+                                            criteria)
+                corners2 = cv2.cornerSubPix(grey_right, corners2, (5,5),
+                                            (-1,-1),
+                                            criteria)
+                objpoints.append(objp)
+                imgpoints_left.append(corners1)
+                imgpoints_right.append(corners2)
+
+    n_images = len(objpoints)
+    n_points = check_rows*check_cols
+    X = np.ones((n_points, 4, n_images))
+    for i in range(n_images):
+        left = correct_dist(np.squeeze(imgpoints_left[i]), fc1, pp1,
+            dc1[[0,1,4]], dc1[[2,3]])
+        right = correct_dist(np.squeeze(imgpoints_right[i]), fc2, pp2,
+            dc2[[0,1,4]], dc2[[2,3]])
+        # left = np.squeeze(imgpoints_left[i])
+        # right = np.squeeze(imgpoints_right[i])
+        points3d = cv2.triangulatePoints(P1, P2, left.T, right.T)
+        X[:,:,i] = np.apply_along_axis(lambda v: v/v[-1],0, points3d).T
+
+    cv2.drawChessboardCorners(img_left,
+                              (check_rows, check_cols),
+                              corners1,
+                              ret1)
+    cv2.drawChessboardCorners(img_right,
+                              (check_rows, check_cols),
+                              corners2,
+                              ret2)
+    cv2.imwrite("{}.jpg".format(directory),
+        np.hstack((img_left, img_right)))
+
+    X = np.mean(X, axis = 2)
+    left_reproj = np.dot(P1, X.T)
+    right_reproj = np.dot(P2, X.T)
+    lr = np.apply_along_axis(lambda v: v/v[-1], 0, left_reproj).T
+    rr = np.apply_along_axis(lambda v: v/v[-1], 0, right_reproj).T
+    errs_left = np.abs(np.squeeze(imgpoints_left[i]) - lr[:,:2])
+    errs_right = np.abs(np.squeeze(imgpoints_right[i]) - rr[:,:2])
+    print("MEAN ERRS L: ", np.mean(errs_left, axis = 0))
+    print("MEAN ERRS R: ", np.mean(errs_right, axis = 0))
+    return X
 
 if __name__ == "__main__":
     directory = r"C:/Users/dhen2714/OneDrive - The University of Sydney (Students)/Phd/Experiments/VolunteerForeheadTracking/Tests/PreSetup/20171220_Calibration_intrinsic/Images"